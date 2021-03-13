e7c7743de9356e289c6c16e1a801a393b0f1770f
denmats <denys.matsuiev@gmail.com>
2021-03-13 20:42:41 +0100

    #5 Implemented InfoBar

--- src/main/java/org/example/MainView.java
+++ src/main/java/org/example/MainView.java
@@ -6,13 +6,15 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.transform.Affine;
 import javafx.scene.transform.NonInvertibleTransformException;
 
 public class MainView extends VBox {
-
+    private InfoBar infoBar;
     private Canvas canvas;
     private Affine affine;
     private Simulation simulation;
@@ -20,56 +22,78 @@ public class MainView extends VBox {
 
     public MainView() {
 
-        this.canvas = new Canvas(400,400);
+        this.canvas = new Canvas(400, 400);
         this.canvas.setOnMousePressed(this::handleDraw);
         this.canvas.setOnMouseDragged(this::handleDraw);
+        this.canvas.setOnMouseMoved(this::handleMoved);
 
         this.setOnKeyPressed(this::getOnKeyPressed);
 
         ToolBar toolBar = new ToolBar(this);
 
-        this.getChildren().addAll(toolBar, this.canvas);
+        this.infoBar = new InfoBar();
+        this.infoBar.setDrawMode(this.drawMode);
+        this.infoBar.setCursorPosition(0, 0);
+
+        Pane spacer = new Pane();
+        spacer.setMinSize(0, 0);
+        spacer.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+
+        this.getChildren().addAll(toolBar, this.canvas, spacer, this.infoBar);
 
         this.affine = new Affine();
-        this.affine.appendScale(400/10f,400/10f);
+        this.affine.appendScale(400 / 10f, 400 / 10f);
 
-        this.simulation = new Simulation(10,10);
+        this.simulation = new Simulation(10, 10);
+    }
+
+    private void handleMoved(MouseEvent mouseEvent) {
+
+        Point2D simulationCoordinates = this.getSimulationCoordinates(mouseEvent);
+
+        this.infoBar.setCursorPosition((int) simulationCoordinates.getX(),(int) simulationCoordinates.getY());
     }
 
     private void getOnKeyPressed(KeyEvent keyEvent) {
-        if(keyEvent.getCode() == KeyCode.D){
+        if (keyEvent.getCode() == KeyCode.D) {
             this.drawMode = Simulation.ALIVE; //Pressing key D set up "drawing mode"
-        }else if(keyEvent.getCode() == KeyCode.E){
+        } else if (keyEvent.getCode() == KeyCode.E) {
             this.drawMode = Simulation.DEAD; //Pressing key E set up "Erasing mode"
         }
     }
 
 
     private void handleDraw(MouseEvent mouseEvent) {
+
+        Point2D simulationCoordinates = this.getSimulationCoordinates(mouseEvent);
+
+        int simX = (int) simulationCoordinates.getX();
+        int simY = (int) simulationCoordinates.getY();
+
+        System.out.println(simX + " , " + simY);
+
+        this.simulation.setState(simX, simY, drawMode);
+        draw();
+    }
+
+    private Point2D getSimulationCoordinates(MouseEvent mouseEvent) {
         double mouseX = mouseEvent.getX();
         double mouseY = mouseEvent.getY();
 
-        System.out.println(mouseX+" , "+mouseY);
-
         try {
             Point2D simulationCoordinates = this.affine.inverseTransform(mouseX, mouseY);
-            int simX = (int) simulationCoordinates.getX();
-            int simY = (int) simulationCoordinates.getY();
-
-            System.out.println(simX+" , "+simY);
-
-            this.simulation.setState(simX,simY,drawMode);
-            draw();
+            return simulationCoordinates;
         } catch (NonInvertibleTransformException e) {
-            System.out.println("Could not revert transform");
+            throw new RuntimeException("Non invertible transform");
         }
     }
 
-    public void draw(){
+    public void draw() {
         GraphicsContext g = this.canvas.getGraphicsContext2D();
 
         g.setFill(Color.LIGHTGRAY);
-        g.fillRect(0,0,400,400);
+        g.fillRect(0, 0, 400, 400);
 
         //set scale ratio for live cells displayed on the board
         g.setTransform(affine);
@@ -78,8 +102,8 @@ public class MainView extends VBox {
         g.setFill(Color.BLACK);
         for (int x = 0; x < this.simulation.width; x++) {
             for (int y = 0; y < this.simulation.height; y++) {
-                if(simulation.getState(x,y) == Simulation.ALIVE){
-                    g.fillRect(x,y,1,1);
+                if (simulation.getState(x, y) == Simulation.ALIVE) {
+                    g.fillRect(x, y, 1, 1);
                 }
             }
         }
@@ -88,10 +112,10 @@ public class MainView extends VBox {
         g.setStroke(Color.GRAY);
         g.setLineWidth(0.05f);
         for (int x = 0; x < this.simulation.width; x++) {
-            g.strokeLine(x,0,x,10);
+            g.strokeLine(x, 0, x, 10);
         }
         for (int y = 0; y < this.simulation.height; y++) {
-            g.strokeLine(0,y,10,y);
+            g.strokeLine(0, y, 10, y);
         }
     }
 
@@ -101,5 +125,6 @@ public class MainView extends VBox {
 
     public void setDrawMode(int newDrawMode) {
         this.drawMode = newDrawMode;
+        this.infoBar.setDrawMode(newDrawMode);
     }
 }
