5381a9cbd8f04f99751e60d0098683ee541abb73
denmats <denys.matsuiev@gmail.com>
2021-03-13 11:52:23 +0100

    #2 Create MainView that displays the simulation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,17 +11,25 @@ import javafx.stage.Stage;
  * JavaFX App: Game of Life.
  * Rules:
  * Any live cell with fewer than two live neighbours dies, as if by underpopulation
+ * Any live cell with two or three live neighbours lives on to the next generation.
+ * Any live cell with more than three live neighbours dies, as if by overpopulation.
+ * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+ *
+ * Any live cell with two or three live neighbours survives.
+ * Any dead cell with three live neighbours becomes a live cell.
+ * All other live cells die in the next generation. Similarly, all other dead cells stay dead.
  *
  */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-
-        Label label = new Label("Hello, JavaFX ");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
+        MainView mainView = new MainView();
+        Scene scene = new Scene(mainView, 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        mainView.draw();
     }
 
     public static void main(String[] args) {
